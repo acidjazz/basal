b0912c9d8046018108a74d70b98728234d8fc56f
kevin olson <acidjazz@gmail.com>
2017-05-11 21:38:18 -0700

    temporarily pull selenium testing until sauce is activated

---
+++ tests/Feature/ClientTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ClientTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testClientList()
+    {
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+    }
+}
