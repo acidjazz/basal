d3147b5fc4767c5c99b5574d62a9a25670dd2c07
Chris Antenesse <chris@antenesse.net>
2017-05-17 23:20:16 -0700

    adds debug stuff: turns on ssh, doesnt start services

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 env
+yum -y install openssh-server passwd
+service sshd start
+echo thePassword | passwd basal --stdin
 cat /var/www/html/.env
-service php-fpm start
-nginx -g 'daemon off;'
-service --status-all
-echo "NGINX LOG"
-cat /var/log/nginx/*
-echo "PHP FPM LOG"
-cat /var/log/php-fpm/7.0/*
+sleep 1000000000
+#service php-fpm start
+#nginx -g 'daemon off;'
+#service --status-all
+#echo "NGINX LOG"
+#cat /var/log/nginx/*
+#echo "PHP FPM LOG"
+#cat /var/log/php-fpm/7.0/*
