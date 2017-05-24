7d1d77cb48e4bf0c41829e59490cea7ac378f1a9
Chris Antenesse <chris@antenesse.net>
2017-05-24 01:52:15 -0500

    adds artisan key gen to startup for unique id per deployment

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,6 +5,7 @@ useradd basal
 echo thePassword | passwd basal --stdin
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd start
+cd /var/www/html && php ./artisan key:generate
 cat /var/www/html/.env
 service php-fpm start
 nginx -g 'daemon off;'
