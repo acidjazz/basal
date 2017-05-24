5728f4cc80b16ea249c333b778c88c0a022f503b
Chris Antenesse <chris@antenesse.net>
2017-05-24 02:22:34 -0500

    adds debuggin tools to the image

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 env
-yum -y install openssh-server passwd
+yum -y install openssh-server passwd vim sudo
 useradd basal
 echo thePassword | passwd basal --stdin
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd start
-cd /var/www/html && php ./artisan key:generate
+echo "basal    ALL=(ALL)       ALL" >> /etc/sudoers
+cd /var/www/html
+export APP_KEY=$(php ./artisan key:generate|awk -F'[' '{print $2}'|awk -F']' '{print $1}')
 cat /var/www/html/.env
 service php-fpm start
 nginx -g 'daemon off;'
