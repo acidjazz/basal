85ad72b933f92d7f4b42a005619c0adf6a395517
Chris Antenesse <chris@antenesse.net>
2017-05-24 00:02:04 -0500

    start basal

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,11 +6,5 @@ echo thePassword | passwd basal --stdin
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd start
 cat /var/www/html/.env
-sleep 1000000000
-#service php-fpm start
-#nginx -g 'daemon off;'
-#service --status-all
-#echo "NGINX LOG"
-#cat /var/log/nginx/*
-#echo "PHP FPM LOG"
-#cat /var/log/php-fpm/7.0/*
+service php-fpm start
+nginx -g 'daemon off;'
